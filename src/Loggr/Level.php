1fe01927db3f92207f31d9d83abd881047dad31f
Alexandre <alexandre@lalalab.com>
2016-08-01 17:21:42 +0200

    Add get_name on Level to get the level as a string

--- src/Loggr/Level.php
+++ src/Loggr/Level.php
@@ -4,15 +4,35 @@ namespace Loggr;
 
 Class Level {
 
-    const EMERGENCY = 5;
-    const ALERT     = 10;
-    const CRITICAL  = 20;
-    const ERROR     = 30;
+    const EMERGENCY = 80;
+    const ALERT     = 70;
+    const CRITICAL  = 60;
+    const ERROR     = 50;
     const WARNING   = 40;
-    const NOTICE    = 50;
-    const INFO      = 60;
-    const DEBUG     = 70;
-    const TIME      = 80;
-    const MEMORY    = 90;
+    const NOTICE    = 30;
+    const INFO      = 20;
+    const DEBUG     = 10;
+    const TIME      = 5;
+    const MEMORY    = 5;
+
+
+    static public function get_name($lvl){
+        switch($lvl){
+            case self::MEMORY    : return 'MEMORY';
+            case self::TIME      : return 'TIME';
+            case self::DEBUG     : return 'DEBUG';
+            case self::INFO      : return 'INFO';
+            case self::NOTICE    : return 'NOTICE';
+            case self::WARNING   : return 'WARNING';
+            case self::ERROR     : return 'ERROR';
+            case self::CRITICAL  : return 'CRITICAL';
+            case self::ALERT     : return 'ALERT';
+            case self::EMERGENCY : return 'EMERGENCY';
+        }
+
+        return '';
+    }
+
+
 
 }
