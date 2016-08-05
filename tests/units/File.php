cfeaa991a3dce60682750e9a97d160f611d7adca
Alexandre <alexandre@lalalab.com>
2016-08-05 15:22:42 +0200

    Create empty atoum test files for loggrs

---
+++ tests/units/File.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Loggr\Tests\Units;
+
+
+require_once __DIR__ . '/../../vendor/bin/atoum';
+use \mageekguy\atoum;
+
+
+class File extends atoum\test {
+
+
+}
---
+++ tests/units/MySQL.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Loggr\Tests\Units;
+
+
+require_once __DIR__ . '/../../vendor/bin/atoum';
+use \mageekguy\atoum;
+
+
+class MySQL extends atoum\test {
+
+
+}
