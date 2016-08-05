fa29350508425b02d6eaf4350a2a055f80d9a7af
Alexandre <alexandre@lalalab.com>
2016-08-05 15:19:01 +0200

    Inplements Console Loggr Modify file to use the FormatableTrait

---
+++ src/Loggr/FormatableTrait.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Loggr;
+
+
+trait FormatableTrait{
+
+    /**
+     * @param $level
+     * @param $message
+     * @param $context
+     * @return mixed|null
+     */
+    protected function _format_message($level, $message, $context) {
+
+        $message = parent::_format_message($message, $context);
+
+        if ($this->_format[$level]) {
+            $format = $this->_format[$level];
+            $log    = parent::_format_message($format, [
+                'level'   => \Loggr\Level::get_name($level),
+                'time'    => parent::_get_time(true),
+                'message' => $message,
+                'context' => ($context ? "\n" . parent::_format_context($context) : ""),
+            ]);
+
+            return $log;
+        }
+        return $message;
+    }
+
+}
